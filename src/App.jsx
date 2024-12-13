dcf95b4ff9c33e73a3c1fb4907562c333355bf11
Ronaldo Ghizi de Souza <ronaldo@stssistemas.com.br>
2024-12-12 23:40:05 -0300

    Cabeçalho do site

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="body">
+        <h1 className="title"> <i className="bi bi-book"></i> Mundo das Palavras</h1>
+        <form className="col-md-3" action="">
+          <div className="form-group">
+
+            <input type="text" className="form-control" id="pesquisaGeral" placeholder="Pesquisar" />
+          </div>
+        </form>
+        <i className="bi bi-person-circle perfilicone"></i>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
